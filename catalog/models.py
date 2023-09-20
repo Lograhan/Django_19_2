8f7f6ab2c68bf3069d7ae2d3781637af7feda361
roman <romanchrnkv@gmail.com>
2023-09-20 21:53:14 +0300

    Выполнено дз 21.1

--- catalog/models.py
+++ catalog/models.py
@@ -16,11 +16,11 @@ class Category(models.Model):
 
 
 class Product(models.Model):
-    p_name = models.CharField(max_length=50, verbose_name='имя')
-    p_description = models.TextField(verbose_name='описание')
-    p_image = models.ImageField(upload_to='product_im/', verbose_name='превью', null=True, blank=True)
-    p_category = models.ForeignKey("Category", on_delete=models.CASCADE, verbose_name='категория')
-    p_price = models.FloatField(verbose_name='цена')
+    p_name = models.CharField(max_length=50, verbose_name='Наименование')
+    p_description = models.TextField(verbose_name='Описание')
+    p_image = models.ImageField(upload_to='product_im/', verbose_name='Превью', null=True, blank=True)
+    p_category = models.ForeignKey("Category", on_delete=models.CASCADE, verbose_name='Категория')
+    p_price = models.FloatField(verbose_name='Цена')
     p_date_creation = models.DateTimeField(auto_now=True)
     p_date_change = models.DateTimeField(auto_now_add=True)
 
@@ -31,3 +31,17 @@ class Product(models.Model):
         verbose_name = 'товар'
         verbose_name_plural = 'товары'
         ordering = ('p_name',)
+
+
+class Version(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    num_version = models.FloatField(verbose_name='номер версии')
+    name = models.CharField(max_length=50, verbose_name='имя версии')
+    is_active = models.BooleanField(default=True, verbose_name='активная версия')
+
+    def __str__(self):
+        return f'{self.product} ({self.name} - {self.num_version})'
+
+    class Meta:
+        verbose_name = 'версия'
+        verbose_name_plural = 'версии'
--- catalog/urls.py
+++ catalog/urls.py
@@ -1,15 +1,19 @@
 from django.urls import path
 
 from catalog.apps import CatalogConfig
-from catalog.views import contacts, ProductCategoryListView, ProductListView, ProductDetailView, CategoryListView, ProductsListView
+from catalog.views import contacts, ProductCategoryListView, ProductListView, ProductDetailView, CategoryListView, \
+    ProductsListView, ProductsCreateView, ProductsUpdateView, ProductDeleteView
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
+    path('create/', ProductsCreateView.as_view(), name='products_create'),
     path('', ProductListView.as_view(), name='index'),
     path('product/<int:pk>/', ProductDetailView.as_view(), name='product'),
     path('contacts/', contacts, name='contacts'),
     path('category/', CategoryListView.as_view(), name='category'),
     path('products/', ProductsListView.as_view(), name='products'),
-    path('products_category/<int:pk>/', ProductCategoryListView.as_view(), name='product_category')
+    path('products_category/<int:pk>/', ProductCategoryListView.as_view(), name='product_category'),
+    path('update/<int:pk>', ProductsUpdateView.as_view(), name='products_update'),
+    path('delete/<int:pk>', ProductDeleteView.as_view(), name='products_delete')
 ]
